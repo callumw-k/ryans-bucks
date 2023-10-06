bc77e533e50e575db388ccd62880f543c2d2f36a
Callum Wellwood-Kane <business@callumwk.com>
2023-10-07 00:22:21 +1100

    updated middleware

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const userToken = request.cookies.get("user");
+  if (!userToken && !request.nextUrl.pathname.startsWith("/enter")) {
+    return NextResponse.redirect(new URL("/enter", request.url));
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
