58c97c6c63a3a27709b178f7ddbad8907742a116
Callum Wellwood-Kane <56098287+callumw-k@users.noreply.github.com>
2023-10-07 16:15:06 +1100

    Update score-field.tsx

--- components/score-field.tsx
+++ components/score-field.tsx
@@ -5,7 +5,7 @@ import { supabase } from "@/lib/supabase";
 import { ScoreWithName } from "./card";
 
 
-function getRandomInt(min, max) {
+function getRandomInt(min:number, max:number) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
